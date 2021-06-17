464a10ff12181342f252c012cb166f0f5ecaed26
fand <fand.gmork@gmail.com>
2021-06-17 17:14:27 +0900

    chore: Remove unused console output

--- docs_src/src/index.tsx
+++ docs_src/src/index.tsx
@@ -5,9 +5,4 @@ import App from "./App";
 import vhCheck from "vh-check";
 vhCheck();
 
-// Print build timestamp
-if (process.env.REACT_APP_BUILD_TIMESTAMP) {
-    console.log(">> Build timestamp:", process.env.REACT_APP_BUILD_TIMESTAMP);
-}
-
 ReactDOM.render(<App />, document.getElementById("root"));
