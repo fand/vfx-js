22c9c8c42b2897cf83c825cd76a30488e87cc086
fand <mail@amagi.dev>
2022-08-30 23:17:59 +0900

    chore: initialize React in v18 way

--- packages/docs/src/index.tsx
+++ packages/docs/src/index.tsx
@@ -1,7 +1,9 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import vhCheck from "vh-check";
 vhCheck();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root")!;
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
