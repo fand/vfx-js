daca415de219f3692c27af060496d16d6e6fc6ee
fand <mail@amagi.dev>
2022-08-31 10:01:10 +0900

    chore: load FXAA from postprocessing package

--- packages/docs2/src/gl/Effects.tsx
+++ packages/docs2/src/gl/Effects.tsx
@@ -1,48 +1,16 @@
-import { useRef, useEffect } from "react";
-import { extend, useThree, useFrame } from "@react-three/fiber";
-// import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
-// import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
-import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader";
+import { useMemo, forwardRef } from "react";
+import { EffectComposer } from "@react-three/postprocessing";
+import { FXAAEffect } from "postprocessing";
 
-import { EffectComposer, SMAA } from "@react-three/postprocessing";
+const FXAA = forwardRef(({}, ref) => {
+    const effect = useMemo(() => new FXAAEffect(), []);
+    return <primitive ref={ref} object={effect} />;
+});
 
 export default function Effects() {
-    const composer = useRef<EffectComposer>();
-    const { scene, gl, size, camera } = useThree();
-
-    useEffect(() => {
-        if (composer.current === undefined) {
-            return;
-        }
-
-        composer.current.setSize(size.width, size.height);
-    }, [size]);
-
-    useFrame(() => {
-        if (composer.current === undefined) {
-            return;
-        }
-
-        composer.current.render();
-    }, 2);
-
     return (
         <EffectComposer multisampling={0}>
-            <SMAA />
+            <FXAA />
         </EffectComposer>
     );
-    // return (
-    //     // <effectComposer ref={composer} args={[gl]}>
-    //     //     <renderPass attachArray="passes" scene={scene} camera={camera} />
-    //     //     <shaderPass
-    //     //         attachArray="passes"
-    //     //         args={[FXAAShader]}
-    //     //         material-uniforms-resolution-value={[
-    //     //             1 / size.width,
-    //     //             1 / size.height,
-    //     //         ]}
-    //     //         renderToScreen
-    //     //     />
-    //     // </effectComposer>
-    // );
 }
