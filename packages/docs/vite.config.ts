4c3b5e2ab9b7926da612e0027624c61cafafe149
fand <mail@amagi.dev>
2024-06-07 12:53:31 -0700

    chore: switch base path by VERCEL

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -4,6 +4,6 @@ import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/react-vfx/",
+    base: process.env.VERCEL ? "" : "/react-vfx/",
     plugins: [react(), viteCommonjs()],
 });
