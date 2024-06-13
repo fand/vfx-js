d4e3f11269c042567d9d1edd18e78f27a42ab3a6
fand <mail@amagi.dev>
2024-06-13 02:25:58 -0700

    Revert "fix: render element only if player is ready"
    This reverts commit b09155157c0279d46f99ee8d0f74013452e00b64.

--- packages/react-vfx/src/element.tsx
+++ packages/react-vfx/src/element.tsx
@@ -1,37 +1,33 @@
 import * as React from "react";
 import { useEffect, useRef, useContext } from "react";
 import { VFXContext } from "./context";
-import type { VFXProps } from "./types";
-import type VFXPlayer from "./vfx-player";
+import { VFXProps } from "./types";
 
 type VFXElementProps<T extends keyof JSX.IntrinsicElements> =
     JSX.IntrinsicElements[T] & VFXProps;
-
-type VFXElementInnerProps<T extends keyof JSX.IntrinsicElements> =
-    VFXElementProps<T> & {
-        player: VFXPlayer;
-        refCallback: (e: HTMLElement) => void;
-    };
 
 function VFXElementFactory<T extends keyof JSX.IntrinsicElements>(
     type: T,
 ): React.ForwardRefExoticComponent<
     React.PropsWithoutRef<VFXElementProps<T>> & React.RefAttributes<HTMLElement>
 > {
-    const VFXElementInner: React.FC<VFXElementInnerProps<T>> = ({
-        player,
-        refCallback,
-        shader,
-        release,
-        uniforms,
-        overflow,
-        ...rawProps
-    }) => {
+    return React.forwardRef(function VFXElement(
+        props: VFXElementProps<T>,
+        parentRef: React.ForwardedRef<HTMLElement>,
+    ) {
+        const player = useContext(VFXContext);
+
         const elementRef = useRef<HTMLElement | undefined>();
         const ref = (e: HTMLElement): void => {
             elementRef.current = e;
-            refCallback(e);
+            if (parentRef instanceof Function) {
+                parentRef(e);
+            } else if (parentRef) {
+                parentRef.current = e;
+            }
         };
+
+        const { shader, release, uniforms, overflow, ...rawProps } = props;
 
         // Create scene
         useEffect(() => {
@@ -40,7 +36,7 @@ function VFXElementFactory<T extends keyof JSX.IntrinsicElements>(
                 return;
             }
 
-            player.addElement(element, {
+            player?.addElement(element, {
                 shader,
                 release,
                 uniforms,
@@ -49,7 +45,7 @@ function VFXElementFactory<T extends keyof JSX.IntrinsicElements>(
 
             const mo = new MutationObserver(() => {
                 if (elementRef.current) {
-                    player.updateTextElement(elementRef.current);
+                    player?.updateTextElement(elementRef.current);
                 }
             });
             mo.observe(element, {
@@ -59,32 +55,12 @@ function VFXElementFactory<T extends keyof JSX.IntrinsicElements>(
             });
 
             return () => {
-                player.removeElement(element);
+                player?.removeElement(element);
                 mo.disconnect();
             };
         }, [elementRef, player, shader, release, uniforms, overflow]);
 
         return React.createElement(type, { ...rawProps, ref });
-    };
-
-    return React.forwardRef(function VFXElement(
-        props: VFXElementProps<T>,
-        parentRef: React.ForwardedRef<HTMLElement>,
-    ) {
-        const player = useContext(VFXContext);
-        if (!player) {
-            return null;
-        }
-
-        const ref = (e: HTMLElement): void => {
-            if (parentRef instanceof Function) {
-                parentRef(e);
-            } else if (parentRef) {
-                parentRef.current = e;
-            }
-        };
-
-        return <VFXElementInner refCallback={ref} player={player} {...props} />;
     });
 }
 
