2063010f9ca473c8a9641090eb4e7d763ef738dd
fand <mail@amagi.dev>
2024-06-21 11:11:52 -0700

    feat: add duotone

--- packages/vfx-js/src/constants.ts
+++ packages/vfx-js/src/constants.ts
@@ -444,6 +444,29 @@ export const shaders = {
         gl_FragColor = texture2D(src, uv);
     }
     `,
+    duotone: `
+    precision mediump float;
+    uniform vec2 resolution;
+    uniform vec2 offset;
+    uniform float time;
+    uniform sampler2D src;
+    uniform vec4 color1;
+    uniform vec4 color2;
+    uniform float speed;
+
+    void main (void) {
+        vec2 uv = (gl_FragCoord.xy - offset) / resolution;
+        vec4 color = texture2D(src, uv);
+
+        float gray = dot(color.rgb, vec3(0.2, 0.7, 0.08));
+        float t = fract(gray * 0.5 + time * speed * 0.5);
+        t = 1. - abs(1. - t * 2.); // fold at 0.5
+
+        gl_FragColor = mix(color1, color2, t);
+
+        gl_FragColor.a *= color.a;
+    }
+    `,
     warpTransition: `
     precision mediump float;
     uniform vec2 resolution;
