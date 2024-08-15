821de9a29440ddc92a0c2995d6cd7648ba7de8e8
fand <mail@amagi.dev>
2024-08-15 15:09:26 -0700

    docs: add stories for presets

--- packages/docs/stories/Header.stories.js
+++ packages/docs/stories/Header.stories.js
@@ -1,28 +1,28 @@
-import { fn } from '@storybook/test';
-import { createHeader } from './Header';
+import { fn } from "@storybook/test";
+import { createHeader } from "./Header";
 
 export default {
-  title: 'Example/Header',
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  render: (args) => createHeader(args),
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
-  },
-  args: {
-    onLogin: fn(),
-    onLogout: fn(),
-    onCreateAccount: fn(),
-  },
-};
-
-export const LoggedIn = {
-  args: {
-    user: {
-      name: 'Jane Doe',
+    title: "Example/Header",
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ["autodocs"],
+    render: (args) => createHeader(args),
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+        layout: "fullscreen",
     },
-  },
+    args: {
+        onLogin: fn(),
+        onLogout: fn(),
+        onCreateAccount: fn(),
+    },
+};
+p;
+export const LoggedIn = {
+    args: {
+        user: {
+            name: "Jane Doe",
+        },
+    },
 };
 
 export const LoggedOut = {};
