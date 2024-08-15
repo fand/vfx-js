0507cd025efb367b36c81733c48b126f6d72d022
fand <mail@amagi.dev>
2024-08-15 11:42:55 -0700

    chore: install storybook

---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,28 @@
+import { join, dirname } from "path";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+    return dirname(require.resolve(join(value, "package.json")));
+}
+
+/** @type { import('@storybook/html-vite').StorybookConfig } */
+const config = {
+    stories: [
+        "../stories/**/*.mdx",
+        "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    ],
+    addons: [
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@chromatic-com/storybook"),
+        getAbsolutePath("@storybook/addon-interactions"),
+    ],
+    framework: {
+        name: getAbsolutePath("@storybook/html-vite"),
+        options: {},
+    },
+};
+export default config;
