0fbdee01624fca6d2971e99b77de5de31532dff9
fand <mail@amagi.dev>
2025-01-28 10:01:18 -0800

    feat: storybook init in docs

---
+++ packages/docs/src/stories/Header.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/html';
+import { fn } from '@storybook/test';
+
+import type { HeaderProps } from './Header';
+import { createHeader } from './Header';
+
+const meta = {
+  title: 'Example/Header',
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  render: (args) => createHeader(args),
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  args: {
+    onLogin: fn(),
+    onLogout: fn(),
+    onCreateAccount: fn(),
+  },
+} satisfies Meta<HeaderProps>;
+
+export default meta;
+type Story = StoryObj<HeaderProps>;
+
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut: Story = {};
