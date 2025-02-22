98081eabc34929764e39979eadb6f5f9ffa4a733
fand <mail@amagi.dev>
2025-02-22 12:43:42 -0800

    chore: move backbuffer stories to a separate file

---
+++ packages/storybook/src/Backbuffer.stories.ts
@@ -0,0 +1,129 @@
+import type { VFXProps, shaders } from "@vfx-js/core";
+import type { Meta, StoryObj } from "@storybook/html";
+
+import { initVFX } from "./utils";
+import Logo from "./assets/logo-640w-20p.svg";
+import "./preset.css";
+
+interface LayoutProps {
+    src?: string;
+    overflow?: number;
+    uniforms?: VFXProps["uniforms"];
+
+    preset: keyof typeof shaders;
+    defaultTime?: number;
+}
+
+export default {
+    title: "Backbuffer",
+    parameters: {
+        layout: "fullscreen",
+    },
+} satisfies Meta<LayoutProps>;
+
+const shader = `
+precision highp float;
+uniform vec2 offset;
+uniform vec2 resolution;
+uniform sampler2D src;
+out vec4 outColor;
+
+void main() {
+    vec2 uv = (gl_FragCoord.xy - offset) / resolution;
+    outColor = texture(src, uv);
+
+    // Show the UV coordinate
+    outColor += vec4(fract(uv), 0, 0.25);
+
+    // Show the center
+    vec2 p = uv * 2. - 1.;
+    p.x *= resolution.x / resolution.y;
+    outColor += (fract(length(p)) * .5 + .5) * 0.25;
+}
+`;
+
+const backbufferShader = `
+precision highp float;
+uniform vec2 offset;
+uniform vec2 resolution;
+uniform float time;
+uniform sampler2D backbuffer;
+out vec4 outColor;
+void main() {
+    vec2 uv = (gl_FragCoord.xy - offset) / resolution;
+    vec2 p = uv * 2. - 1.;
+    p.x *= resolution.x / resolution.y;
+
+    p.x -= (time / 30. * 2.) - 1.;
+
+    outColor = vec4(step(length(p), .3));
+    outColor += texture(backbuffer, uv) * vec4(.9, .95, 1, .995);
+}
+`;
+
+export const backbuffer: StoryObj<undefined> = {
+    render: () => {
+        const img = document.createElement("img");
+        img.src = Logo;
+        return img;
+    },
+    args: undefined,
+};
+backbuffer.play = async ({ canvasElement }) => {
+    const img = canvasElement.querySelector("img") as HTMLImageElement;
+    await new Promise((o) => {
+        img.onload = o;
+    });
+
+    let time = 0;
+
+    const vfx = initVFX({ autoplay: false });
+    await vfx.add(img, {
+        shader: backbufferShader,
+        backbuffer: true,
+        uniforms: { time: () => time },
+    });
+
+    while (time < 30) {
+        time++;
+        vfx.render();
+    }
+};
+
+export const backbufferCompatibility: StoryObj<undefined> = {
+    render: () => {
+        const wrapper = document.createElement("div");
+        wrapper.className = "backbufferWrapper";
+
+        const img1 = document.createElement("img");
+        img1.id = "img1";
+        img1.src = Logo;
+        wrapper.appendChild(img1);
+
+        const img2 = document.createElement("img");
+        img2.id = "img2";
+        img2.src = Logo;
+        wrapper.appendChild(img2);
+
+        return wrapper;
+    },
+    args: undefined,
+};
+backbufferCompatibility.play = async ({ canvasElement }) => {
+    const img1 = canvasElement.querySelector("#img1") as HTMLImageElement;
+    const img2 = canvasElement.querySelector("#img2") as HTMLImageElement;
+    await Promise.all([
+        new Promise((o) => {
+            img1.onload = o;
+        }),
+        new Promise((o) => {
+            img2.onload = o;
+        }),
+    ]);
+
+    const vfx = initVFX({ autoplay: false });
+    await vfx.add(img1, { shader, backbuffer: false });
+    await vfx.add(img2, { shader, backbuffer: true });
+
+    vfx.render();
+};
