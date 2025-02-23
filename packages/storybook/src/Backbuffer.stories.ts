e35b859e1f931ba601521bca89366a2d56dd73c7
fand <mail@amagi.dev>
2025-02-23 01:21:15 -0800

    docs: add story for compat test in fullscreen mode

--- packages/storybook/src/Backbuffer.stories.ts
+++ packages/storybook/src/Backbuffer.stories.ts
@@ -61,6 +61,32 @@ void main() {
 }
 `;
 
+const shaderFullscreen = `
+precision highp float;
+uniform vec2 offset;
+uniform vec2 resolution;
+uniform sampler2D src;
+uniform vec4 rectOuter;
+uniform int id;
+out vec4 outColor;
+
+void main() {
+    if (int(gl_FragCoord.x / 100.) % 2 == id)  {
+        discard;
+    }
+
+    vec2 uv = (gl_FragCoord.xy - offset) / resolution;
+    outColor = texture(src, uv);
+
+    // Show the UV coordinate
+    outColor += vec4(gl_FragCoord.xy / rectOuter.zw, 1, 0.5);
+
+    vec2 p = gl_FragCoord.xy / rectOuter.zw * 2. - 1.;
+    p.x *= rectOuter.z / rectOuter.w;
+    outColor += smoothstep(.1, .0, abs(sin(length(p) * 30.))) * 0.1;
+}
+`;
+
 export const backbuffer: StoryObj<undefined> = {
     render: () => {
         const img = document.createElement("img");
@@ -124,6 +150,92 @@ backbufferCompatibility.play = async ({ canvasElement }) => {
     const vfx = initVFX({ autoplay: false });
     await vfx.add(img1, { shader, backbuffer: false });
     await vfx.add(img2, { shader, backbuffer: true });
+
+    vfx.render();
+};
+
+export const backbufferCompatibilityOverflow: StoryObj<undefined> = {
+    render: () => {
+        const wrapper = document.createElement("div");
+        wrapper.className = "backbufferWrapper";
+
+        const img1 = document.createElement("img");
+        img1.id = "img1";
+        img1.src = Logo;
+        wrapper.appendChild(img1);
+
+        const img2 = document.createElement("img");
+        img2.id = "img2";
+        img2.src = Logo;
+        wrapper.appendChild(img2);
+
+        return wrapper;
+    },
+    args: undefined,
+};
+backbufferCompatibilityOverflow.play = async ({ canvasElement }) => {
+    const img1 = canvasElement.querySelector("#img1") as HTMLImageElement;
+    const img2 = canvasElement.querySelector("#img2") as HTMLImageElement;
+    await Promise.all([
+        new Promise((o) => {
+            img1.onload = o;
+        }),
+        new Promise((o) => {
+            img2.onload = o;
+        }),
+    ]);
+
+    const vfx = initVFX({ autoplay: false });
+    await vfx.add(img1, { shader, backbuffer: false, overflow: 30 });
+    await vfx.add(img2, { shader, backbuffer: true, overflow: 30 });
+
+    vfx.render();
+};
+
+export const backbufferCompatibilityFullscreen: StoryObj<undefined> = {
+    render: () => {
+        const wrapper = document.createElement("div");
+        wrapper.className = "backbufferWrapper";
+
+        const img1 = document.createElement("img");
+        img1.id = "img1";
+        img1.src = Logo;
+        wrapper.appendChild(img1);
+
+        const img2 = document.createElement("img");
+        img2.id = "img2";
+        img2.src = Logo;
+        wrapper.appendChild(img2);
+
+        return wrapper;
+    },
+    args: undefined,
+};
+backbufferCompatibilityFullscreen.play = async ({ canvasElement }) => {
+    const img1 = canvasElement.querySelector("#img1") as HTMLImageElement;
+    const img2 = canvasElement.querySelector("#img2") as HTMLImageElement;
+    await Promise.all([
+        new Promise((o) => {
+            img1.onload = o;
+        }),
+        new Promise((o) => {
+            img2.onload = o;
+        }),
+    ]);
+
+    const vfx = initVFX({ autoplay: false });
+    await vfx.add(img1, {
+        shader: shaderFullscreen,
+        backbuffer: false,
+        overflow: true,
+        uniforms: { id: 0 },
+    });
+    await vfx.add(img2, {
+        shader: shaderFullscreen,
+        backbuffer: true,
+        overflow: true,
+        uniforms: { id: 1 },
+    });
 
     vfx.render();
 };
