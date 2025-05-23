784ba8d4d98724dd34a570d554a5ad942d04e4ad
fand <mail@amagi.dev>
2025-05-23 16:55:27 -0700

    test: add story to check horizontal scroll layout

--- packages/storybook/src/ImageScroll.stories.ts
+++ packages/storybook/src/ImageScroll.stories.ts
@@ -22,10 +22,7 @@ export const longPageWithImage: StoryObj = {
         // Place the image somewhere in the middle
         const img = document.createElement("img");
         img.src = Logo;
-        img.style.display = "block";
         img.style.margin = "900px auto 0 auto";
-        img.style.maxWidth = "400px";
-        img.style.boxShadow = "0 4px 32px #0008";
 
         container.appendChild(img);
 
@@ -42,6 +39,40 @@ export const longPageWithImage: StoryObj = {
         return container;
     },
     name: "Image in Long Scrollable Page",
+};
+
+export const longPageWithImageHorizontal: StoryObj = {
+    render: () => {
+        // Create a tall container to force vertical scroll
+        const container = document.createElement("div");
+        container.style.height = "2000px";
+        container.style.position = "relative";
+        container.style.background = "#111";
+
+        // Place the image somewhere in the middle
+        const img = document.createElement("img");
+        img.src = Logo;
+        img.style.minWidth = "3000px";
+
+        img.style.margin = "900px auto 0 auto";
+
+        container.appendChild(img);
+
+        // Use Timer to mock time for VRT
+        const timer = new Timer(1.0, [0, 10]);
+        container.appendChild(timer.element);
+
+        const vfx = initVFX();
+        vfx.add(img, {
+            shader: "sinewave",
+            uniforms: { time: () => timer.time },
+        }).then(() => {
+            img.style.opacity = "0.4";
+        });
+
+        return container;
+    },
+    name: "Image in Long Scrollable Page X",
 };
 
 export const scrolledToImage: StoryObj = {
