aed8631db898e521d2e25ac10970be1302cd4bcf
fand <fand@users.noreply.github.com>
2026-04-09 17:08:03 +0000

    deploy: 34714c15e0baf26d513f8dfa9cd6b73e48336298

--- assets/test-CHQRy-P1.js
+++ assets/test-CHQRy-P1.js
@@ -1,4 +1,4 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.js";const a=`
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as h}from"./vfx-CLlr3OTc.js";const C="modulepreload",S=function(e,t){return new URL(e,t).href},y={},_=function(t,r,c){let i=Promise.resolve();if(r&&r.length>0){let v=function(o){return Promise.all(o.map(n=>Promise.resolve(n).then(f=>({status:"fulfilled",value:f}),f=>({status:"rejected",reason:f}))))};const l=document.getElementsByTagName("link"),a=document.querySelector("meta[property=csp-nonce]"),g=(a==null?void 0:a.nonce)||(a==null?void 0:a.getAttribute("nonce"));i=v(r.map(o=>{if(o=S(o,c),o in y)return;y[o]=!0;const n=o.endsWith(".css"),f=n?'[rel="stylesheet"]':"";if(!!c)for(let m=l.length-1;m>=0;m--){const x=l[m];if(x.href===o&&(!n||x.rel==="stylesheet"))return}else if(document.querySelector(`link[href="${o}"]${f}`))return;const u=document.createElement("link");if(u.rel=n?"stylesheet":C,n||(u.as="script"),u.crossOrigin="",u.href=o,g&&u.setAttribute("nonce",g),document.head.appendChild(u),n)return new Promise((m,x)=>{u.addEventListener("load",m),u.addEventListener("error",()=>x(new Error(`Unable to preload CSS for ${o}`)))})}))}function s(v){const l=new Event("vite:preloadError",{cancelable:!0});if(l.payload=v,window.dispatchEvent(l),!l.defaultPrevented)throw v}return i.then(v=>{for(const l of v||[])l.status==="rejected"&&s(l.reason);return t().catch(s)})},w=/iPad|iPhone/.test(navigator.userAgent)||navigator.platform==="MacIntel"&&navigator.maxTouchPoints>1;w&&_(async()=>{const{default:e}=await import("https://esm.sh/lenis@1.1.20");return{default:e}},[],import.meta.url).then(({default:e})=>{new e({autoRaf:!0,syncTouch:!0})});const R=`
   precision highp float;
   uniform sampler2D src;
   uniform vec2 resolution;
@@ -7,7 +7,7 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
   void main() {
       vec2 uv = (gl_FragCoord.xy - offset) / resolution;
       outColor = texture(src, uv);
-  }`,s=`
+  }`,D=`
   precision highp float;
   uniform sampler2D velocity;
   uniform vec2 resolution;
@@ -21,7 +21,7 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
       float T = texture(velocity, uv + vec2(0.0, t.y)).x;
       float B = texture(velocity, uv - vec2(0.0, t.y)).x;
       outColor = vec4(0.5 * (R - L - T + B), 0.0, 0.0, 1.0);
-  }`,n=`
+  }`,L=`
   precision highp float;
   uniform sampler2D velocity;
   uniform sampler2D curl;
@@ -57,7 +57,7 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
       float mSplat = exp(-dot(diff, diff) / splatRadius);
       vel += (mouseDelta / resolution) * mSplat * splatForce;
       outColor = vec4(vel, 0.0, 1.0);
-  }`,f=`
+  }`,E=`
   precision highp float;
   uniform sampler2D vort_vel;
   uniform vec2 resolution;
@@ -76,10 +76,10 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
       if (uv.y + t.y > 1.0) T = -C.y;
       if (uv.y - t.y < 0.0) B = -C.y;
       outColor = vec4(0.5 * (R - L + T - B), 0.0, 0.0, 1.0);
-  }`,m=`
+  }`,T=`
   precision highp float;
   out vec4 outColor;
-  void main() { outColor = vec4(0.0); }`,x=`
+  void main() { outColor = vec4(0.0); }`,b=`
   precision highp float;
   uniform sampler2D src;
   uniform sampler2D divergence;
@@ -95,7 +95,7 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
       float B = texture(src, uv - vec2(0.0, t.y)).x;
       float div = texture(divergence, uv).x;
       outColor = vec4((L + R + B + T - div) * 0.25, 0.0, 0.0, 1.0);
-  }`;function d(e){return`
+  }`;function F(e){return`
   precision highp float;
   uniform sampler2D vort_vel;
   uniform sampler2D ${e};
@@ -112,7 +112,7 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
       vec2 vel = texture(vort_vel, uv).xy;
       vel -= vec2(R - L, T - B);
       outColor = vec4(vel, 0.0, 1.0);
-  }`}const p=`
+  }`}const P=`
   precision highp float;
   uniform sampler2D proj_vel;
   uniform vec2 resolution;
@@ -128,7 +128,7 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
       advected /= 1.0 + velocityDissipation * 0.016;
 
       outColor = vec4(advected, 0.0, 1.0);
-  }`,g=`
+  }`,I=`
   precision highp float;
   uniform sampler2D velocity;
   uniform sampler2D canvas;
@@ -137,7 +137,6 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
   uniform vec2 simSize;
   uniform float time;
   out vec4 outColor;
-
   vec3 spectrum(float x) {
     return cos((x - vec3(0, .5, 1)) * vec3(.6, 1., .5) * 3.14);
   }
@@ -147,12 +146,25 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{V as l}from"./vfx-Ct8kENVT.j
     vec2 vel = texture(velocity, uv).xy;
 
     vec2 disp = vel / simSize;
-    vec2 cr = texture(canvas, uv - disp * 0.050).ra;
-    vec2 cg = texture(canvas, uv - disp * 0.040).ga;
-    vec2 cb = texture(canvas, uv - disp * 0.030).ba;
-    outColor = vec4(cr.x, cg.x, cb.x, (cr.y + cg.y + cb.y) / 3.);
-
     float v = length(disp);
-    outColor += vec4(spectrum(sin(v * 3. + time) * 0.4 + 0.5), 1) * smoothstep(.2, .8, v)
-   * 0.5;
-  }`;function y(e){const{simSize:t,mouseDelta:o}=e,u=[];u.push({frag:m,target:"p_a",float:!0,size:t});let r="p_a";for(let c=0;c<e.pressureIterations;c++)r=c%2===0?"p_b":"p_a",u.push({frag:x,target:r,float:!0,size:t});return[{frag:a,target:"canvas"},{frag:s,target:"curl",float:!0,size:t},{frag:n,target:"vort_vel",float:!0,size:t,uniforms:{mouseDelta:o,curlStrength:e.curlStrength,splatForce:e.splatForce,splatRadius:e.splatRadius}},{frag:f,target:"divergence",float:!0,size:t},...u,{frag:d(r),target:"proj_vel",float:!0,size:t},{frag:p,target:"velocity",persistent:!0,float:!0,size:t,uniforms:{velocityDissipation:e.velocityDissipation}},{frag:g,uniforms:{time:e.time,simSize:t}}]}const h=document.getElementById("app");let i=[-1,-1],v=[0,0];window.addEventListener("mousemove",e=>{const t=e.clientX,o=window.innerHeight-e.clientY;i[0]>=0&&(v=[t-i[0],o-i[1]]),i=[t,o]});window.addEventListener("load",async()=>{const t=document.documentElement.clientWidth/document.documentElement.clientHeight,o=t>1?[Math.round(128*t),128]:[128,Math.round(128/t)],u=y({simSize:o,mouseDelta:()=>(v=[v[0]*.9,v[1]*.9],v),pressureIterations:8,curlStrength:20,velocityDissipation:1,splatForce:3e3,splatRadius:.001,time:()=>performance.now()/1e3}),r=new l({postEffect:u});await r.add(h,{shader:"none"}),r.play()});
+
+    // dispersion
+    const int N = 8;
+    vec4 c = vec4(0.0);
+    vec3 wsum = vec3(0.0);
+    for (int i = 0; i < N; i++) {
+      float t = float(i) / float(N - 1);
+      vec3 w = max(vec3(0.0),
+      cos((t - vec3(0.0, 0.5, 1.0)) * 3.14159 * 0.5));
+      vec4 s = texture(canvas, uv - disp * 0.3 * (t + 0.3) * v);
+      c.rgb += s.rgb * w;
+      c.a   += s.a * (w.r + w.g + w.b) / 3.0;
+      wsum  += w;
+    }
+    c.rgb /= wsum;
+    c.a /= (wsum.r + wsum.g + wsum.b) / 3.0;
+    outColor = c;
+
+    vec4 c2 = vec4(spectrum(sin(v * 2.) * 0.4 + 0.6), 1);
+    outColor += c2 * smoothstep(.2, .8, v) * 0.5;
+  }`;function z(e){const{simSize:t,mouseDelta:r}=e,c=[];c.push({frag:T,target:"p_a",float:!0,size:t});let i="p_a";for(let s=0;s<e.pressureIterations;s++)i=s%2===0?"p_b":"p_a",c.push({frag:b,target:i,float:!0,size:t});return[{frag:R,target:"canvas"},{frag:D,target:"curl",float:!0,size:t},{frag:L,target:"vort_vel",float:!0,size:t,uniforms:{mouseDelta:r,curlStrength:e.curlStrength,splatForce:e.splatForce,splatRadius:e.splatRadius}},{frag:E,target:"divergence",float:!0,size:t},...c,{frag:F(i),target:"proj_vel",float:!0,size:t},{frag:P,target:"velocity",persistent:!0,float:!0,size:t,uniforms:{velocityDissipation:e.velocityDissipation}},{frag:I,uniforms:{time:e.time,simSize:t}}]}const B=document.getElementById("app");let p=[-1,-1],d=[0,0];window.addEventListener("pointermove",e=>{const t=e.clientX,r=window.innerHeight-e.clientY;p[0]>=0&&(d=[t-p[0],r-p[1]]),p=[t,r]});window.addEventListener("load",async()=>{const t=document.documentElement.clientWidth/document.documentElement.clientHeight,r=t>1?[Math.round(128*t),128]:[128,Math.round(128/t)],c=z({simSize:r,mouseDelta:()=>(d=[d[0]*.9,d[1]*.9],d),pressureIterations:8,curlStrength:20,velocityDissipation:1,splatForce:3e3,splatRadius:.001,time:()=>performance.now()/1e3}),i=new h({postEffect:c});await i.add(B,{shader:"none"}),i.play()});
