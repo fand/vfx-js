78e3e0d3979f7d3eb5f9b758156d3e1f6bebbc94
fand <fand@users.noreply.github.com>
2026-06-08 05:49:18 +0000

    deploy: 14ba11bfd60dfd9fd298db90c3625fe166b8ddff

---
+++ storybook/assets/Mipmap.stories-CC5WutSn.js
@@ -0,0 +1,50 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{l as t,u as n}from"./utils-C8nz1GgH.js";import{t as r}from"./preset-B7f9t9lo.js";import{n as i,t as a}from"./jellyfish-CbdkhNBT.js";var o,s,c,l,u,d;e((()=>{a(),r(),n(),o=`#version 300 es
+precision highp float;
+in vec2 uvSrc;
+out vec4 outColor;
+uniform sampler2D src;
+void main() {
+    outColor = texture(src, uvSrc);
+}
+`,s=`#version 300 es
+precision highp float;
+in vec2 uv;
+out vec4 outColor;
+uniform sampler2D rt;
+uniform float lod;
+void main() {
+    float useLod = uv.x < 0.5 ? 0.0 : lod;
+    vec4 c = textureLod(rt, uv, useLod);
+    // Red separator down the middle for unambiguous split.
+    if (abs(uv.x - 0.5) < 0.002) c = vec4(1.0, 0.0, 0.0, 1.0);
+    outColor = c;
+}
+`,c=class{lod;#e=null;constructor(e){this.lod=e}init(e){this.#e=e.createRenderTarget({mipmap:!0})}render(e){this.#e&&(e.draw({frag:o,uniforms:{src:e.src},target:this.#e}),e.draw({frag:s,uniforms:{rt:this.#e,lod:this.lod},target:e.target}))}dispose(){this.#e=null}},l={title:`Mipmap`,parameters:{layout:`fullscreen`}},u={render:e=>{let n=document.createElement(`img`);n.src=i,n.style.display=`block`,n.style.margin=`40px auto`;let r=t(),a=new c(e.lod);return n.onload=()=>{r.add(n,{effect:a})},n},args:{lod:4},argTypes:{lod:{control:{type:`range`,min:0,max:10,step:.1}}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: args => {
+    const img = document.createElement("img");
+    img.src = Jellyfish;
+    img.style.display = "block";
+    img.style.margin = "40px auto";
+    const vfx = initVFX();
+    const effect = new MipmapSmokeEffect(args.lod);
+    img.onload = () => {
+      vfx.add(img, {
+        effect
+      });
+    };
+    return img;
+  },
+  args: {
+    lod: 4
+  },
+  argTypes: {
+    lod: {
+      control: {
+        type: "range",
+        min: 0,
+        max: 10,
+        step: 0.1
+      }
+    }
+  }
+}`,...u.parameters?.docs?.source}}},d=[`smoke`]}))();export{d as __namedExportsOrder,l as default,u as smoke};
